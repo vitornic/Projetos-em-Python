06c4a1b390879300e39243e61d6017c81e72845e
João Vitor <34252083+vitornic@users.noreply.github.com>
2018-03-21 20:00:17 -0300

    Update Ex04.py

--- Ex04.py
+++ Ex04.py
@@ -1,8 +1,6 @@
 KmTotal = input("Informe a distância percorrida ")
 LitroComb = input("Informe o litro de combustível utilizado ")
 
-KmTotal_int = int(KmTotal)
-LitroComb_int = int(LitroComb)
-media = KmTotal_int / LitroComb_int
+media = (float(KmTotal) / float(LitroComb))
 
 print("A média entre a distância ",KmTotal," km e o gasto de combustível ",LitroComb," litros é: ",media )
