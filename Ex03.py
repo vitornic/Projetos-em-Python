332a4561152a64e60f86f6f712a2d59bdf92a889
João Vitor <34252083+vitornic@users.noreply.github.com>
2018-03-21 19:43:33 -0300

    Update Ex03.py

--- Ex03.py
+++ Ex03.py
@@ -1,4 +1,4 @@
 num1 = input("Digite um número ")
 num2 = input("Digite um segundo número ")
-soma = (int(num1)+int(num2))
+soma = (float(num1)+floatt(num2))
 print("A soma dos números ", num1 ,"+", num2 ,"é : ", soma)
