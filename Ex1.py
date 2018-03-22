b46b7d09bf9c392e4c14cc51efd2b5ac011a12a5
João Vitor <34252083+vitornic@users.noreply.github.com>
2018-03-21 21:58:11 -0300

    Update Ex1.py

--- Ex1.py
+++ Ex1.py
@@ -1,6 +1,6 @@
-num = int('Digite um numero')
-if (num % 2 == 0)
-  print('O numero digita é par')
-  print('O numero e: ',num)
- else:
+num = int(input('Digite um numero'))
+if (num % 2 == 0):
+    print('O numero digita é par')
+    print('O numero e: ',num)
+else:
     print('O numero é impar')
