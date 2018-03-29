a703e79e006b84ffcc00bddc30c1489f7f75f0b8
João Vitor <34252083+vitornic@users.noreply.github.com>
2018-03-28 22:11:53 -0300

    Update 2803Ex08.py

--- 2803Ex08.py
+++ 2803Ex08.py
@@ -1,13 +1,13 @@
 min = float(input('Minutos'))
-if (min < 200):
+if (min < 200) and (min < 400):
     print('Isento')
     print('Minutos', min)
-    print('taxa 31.14')
+    print('Taxa',(min)+31.14)
 elif (min >= 200):
-    taxa = (min * 0.02)
+    taxa = (min * 0.02) + 31.14
     print('Minutos', min)
     print('Taxa: ',taxa)
 elif (min >= 400):
-    taxa = (min * 0.01)
+    taxa = (min * 0.01) + 31.14
     print('Minutos', min)
     print('Taxa',taxa)
