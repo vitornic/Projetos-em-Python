78f7b276ddf468aed3a9d11ecace3b5402f8e2f5
João Vitor <34252083+vitornic@users.noreply.github.com>
2018-04-11 19:19:36 -0300

    Create Ex091104.py

---
+++ Ex091104.py
@@ -0,0 +1,10 @@
+n1 = float(input('Numero 1 '))
+n2 = float(input('Numero 2 '))
+n3 = float(input('Numero 3 '))
+media = (n1+n2+n3)/3
+if media >= 6.0:
+    print('Aprovado')
+elif media >= 4.0:
+    print('Sub')
+else:
+    print('Notas insuficientes')
